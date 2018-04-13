1a622c8e1059c703db863e089d80f6163a0c0bad
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-13 17:47:47 +0900

    rootパスをgroupsコントローラに変更。これに伴って、ビューファイルパスを変更。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,9 @@
 class GroupsController < ApplicationController
   before_action :get_group, only: [:edit, :update]
 
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
