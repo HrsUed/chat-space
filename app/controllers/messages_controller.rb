7a80202bbb04f9f42a090433e2c568611b084683
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-14 18:58:31 +0900

    メッセージテキストのみ送信可能な状態にcreateアクションを実装（イメージはまだ）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
+
   def index
-    @group = Group.find(params[:group_id])
     @messages = @group.messages
     @members = @group.users.inject([]) { |members, user| members << user.name }
   end
 
   def create
+    @messages = @group.messages
+    if @messages << Message.new(message_params)
+      redirect_to group_messages_path(@group)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge({group_id: params[:group_id], user_id: current_user.id})
   end
 end
