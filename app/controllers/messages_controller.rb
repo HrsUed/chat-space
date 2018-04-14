bc68343dc75b3e0cea5e8817a7597482dc3b215f
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-14 17:07:50 +0900

    messagesコントローラをgroupsコントローラにネストしたため、ビューの構造を変更。 ・groups#indexはグループ未選択状態 ・サイドバーからグループ選択すると、messages#index表示 ・messages#indexにてヘッダーに選択されたグループの情報を表示 ・Editボタンへの正式なリンクを割り当て

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,8 @@
 class MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    @members = @group.users.inject([]) { |members, user| members << user.name }
   end
 
   def create
