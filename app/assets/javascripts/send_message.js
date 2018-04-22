2d335f9bb463e67c244574e52f2dbb95c2629e3a
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-22 19:03:35 +0900

    メッセージ送信用jsファイルの実装完了

--- app/assets/javascripts/send_message.js
+++ app/assets/javascripts/send_message.js
@@ -1,4 +1,30 @@
 $(function() {
+  function buildHTML(message) {
+    var html = `<div class="message-header">
+                  <div class="message-header__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="message-header__posted-date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="message-body">`
+
+    if (message.content != "") {
+      html += `<div class="message-body__content">
+                ${message.content}
+              </div>`
+    }
+    if (message.image != null) {
+      html += `<div class="message-body__image">
+                 <img src="${message.image}">
+               </div>`
+    }
+    html += `</div>`
+    return html;
+  }
+
+
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -13,10 +39,11 @@ $(function() {
       contentType: false
     })
     .done(function(data){
-
+      var html = buildHTML(data);
+      $(".messages").append(html);
     })
     .fail(function(){
-
+      alert("メッセージの送信に失敗しました。");
     });
   });
 });
