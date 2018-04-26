9ff14ec65ad7252b165692f5763d8b40a9633d1a
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-26 18:26:35 +0900

    グループ編集ビューにてユーザのインクリメンタルサーチの実装完了（検索まで）

--- app/assets/javascripts/search_users.js
+++ app/assets/javascripts/search_users.js
@@ -1,22 +1,44 @@
 $(function() {
-  $(".edit_group").on("keyup", "#user-search-field", function(e) {
+  const resultSelector = $("#user-search-result");
+
+  function buildHTML(user) {
+    var html =
+`
+<div class="chat-group-user clearfix">
+  <p class="chat-group-user__name">${user.name}</p>
+  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+</div>
+`
+    return html;
+  }
+
+  // ユーザのインクリメンタルサーチ
+  $(".edit_group").on("keyup", "#user-search-field", function() {
     var keyword = $("#user-search-field").val();
+    console.log(keyword);
 
-    $.ajax({
-      url: "/users",
-      type: "GET",
-      data: { keyword: keyword },
-      dataType: "json",
-      contentType: false
-    })
-    .done(function(names){
-      console.log(names);
-    })
-    .fail(function(){
-
-    })
-    .always(function(){
-
-    });
+    if (keyword == "") {
+      resultSelector.empty();
+    } else {
+      $.ajax({
+        url: "/users",
+        type: "GET",
+        data: { keyword: keyword },
+        dataType: "json",
+        contentType: false
+      })
+      .done(function(users){
+        if (users.length > 0) {
+          $("#user-search-result").empty();
+          users.forEach(function(user) {
+            resultSelector.append(buildHTML(user));
+          });
+        }
+      })
+      .fail(function(){
+        alert("ユーザ検索に失敗しました。");
+      });
+    }
   });
+
 });
