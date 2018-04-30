0dcc1d8259597a7b304ae783e365dadcbdcfaf87
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-05-01 01:37:24 +0900

    レビュー指摘修正（未取り込みのメッセージのみ取得するように変更） 取り込み済みのメッセージの最終IDをクエリに指定するように変更。受信したメッセージは全て未取り込みのため、それらを全て表示するように変更。

--- app/assets/javascripts/auto_update.js
+++ app/assets/javascripts/auto_update.js
@@ -4,18 +4,18 @@ $(function() {
     if ($("div").hasClass("messages")) {
       var targetSelector = $(".messages");
 
+      var latest_message_id = $(".message:last-child").data("message-id");
+
       $.ajax({
         url: location.href,
         type: "GET",
+        data: { latest_message_id: latest_message_id },
         dataType: "json",
-        processData: false,
         contentType: false
       })
       .done(function(messages) {
         messages.forEach(function(message) {
-          if ($("#message-id-" + message.id).val() == undefined) {
-            targetSelector.append(buildMessageHTML(message));
-          }
+          targetSelector.append(buildMessageHTML(message));
         });
         targetSelector.animate({scrollTop: targetSelector.get(0).scrollHeight});
       })
