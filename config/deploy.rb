3a9c2479f9a0d7708d1515255307b75aba8b227d
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-30 14:24:05 +0900

    Capistranoのconfig/deploy.rbの誤記を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:Hermiltonian/chat-space.git"
+set :repo_url, "git@github.com:Hermiltonian/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
