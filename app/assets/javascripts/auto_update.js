b02c3aa0d4b6046c8dd521eabe31b7ee3ac3cc16
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-05-03 11:07:42 +0900

    本番環境で一時的にセッションタイムアウトさせるための変更

--- app/assets/javascripts/auto_update.js
+++ app/assets/javascripts/auto_update.js
@@ -1,30 +1,30 @@
-$(function() {
-  function updateMessages() {
+// $(function() {
+//   function updateMessages() {
 
-    if ($("div").hasClass("messages")) {
-      var targetSelector = $(".messages");
+//     if ($("div").hasClass("messages")) {
+//       var targetSelector = $(".messages");
 
-      var latest_message_id = $(".message:last-child").data("message-id");
+//       var latest_message_id = $(".message:last-child").data("message-id");
 
-      $.ajax({
-        url: location.href,
-        type: "GET",
-        data: { latest_message_id: latest_message_id },
-        dataType: "json",
-        contentType: false
-      })
-      .done(function(messages) {
-        messages.forEach(function(message) {
-          targetSelector.append(buildMessageHTML(message));
-        });
-        targetSelector.animate({scrollTop: targetSelector.get(0).scrollHeight});
-      })
-      .fail(function() {
+//       $.ajax({
+//         url: location.href,
+//         type: "GET",
+//         data: { latest_message_id: latest_message_id },
+//         dataType: "json",
+//         contentType: false
+//       })
+//       .done(function(messages) {
+//         messages.forEach(function(message) {
+//           targetSelector.append(buildMessageHTML(message));
+//         });
+//         targetSelector.animate({scrollTop: targetSelector.get(0).scrollHeight});
+//       })
+//       .fail(function() {
 
-      });
-    }
-  }
+//       });
+//     }
+//   }
 
-  setInterval(updateMessages, 5000);
+//   setInterval(updateMessages, 5000);
 
-});
+// });
